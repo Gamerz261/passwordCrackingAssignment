f564412fcd0f53b387d2e163971c7c2f5df4e3ab
stephencupitt <s2cupitt@gmail.com>
2022-11-15 08:00:02 -0600

    Program can now input text files 'n such

--- README.md
+++ README.md
@@ -12,12 +12,14 @@ Coded in Python, As assigned by the 2022 CSHS Cybersecurity course
 - [ ] SHA-256 hashed passwords can be checked (10)
 - [ ] Brute force approach option (10)
 - [ ] Dictionary Attack option (10)
-- [ ] Command line arguments can be taken in (10) 
+- [X] Command line arguments can be taken in (10) 
 
 ### FLAGS
 
 ```
 -h               // HELP 
+-i               // INPUT FILE
+-o               // OUTPUT FILE
 /** ARGUMENTS **/
 ```
 
--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 # Import all methods from the method file -- Also runs the file
-from methods import *
-import getopt 
+#///from methods import *
+import sys, getopt
 
 mode = ""
 
-def print_hi(name):
-    # Sample code
-    print(f'Hi, {name}')
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print('test.py -i <inputfile> -o <outputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('test.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print('Input file is "', inputfile)
+   print('Output file is "', outputfile)
 
-
-
-# Variables
-if __name__ == '__main__':
-    print_hi('Hello')
+if __name__ == "__main__":
+   main(sys.argv[1:])
