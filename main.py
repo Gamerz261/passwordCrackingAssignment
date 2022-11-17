7eb7978e302afce6005a52a27c5c75a63a033560
stephencupitt <s2cupitt@gmail.com>
2022-11-17 10:31:10 -0600

    Updated main.py with more initial variables and deleted legacy args

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 #///from methods import *
 import sys, getopt
 
+
 mode = []
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
+   # Initilize variables for use in running the proper method for encrypting or decrypting the password
+   type = ''
+   password = ''
+   hash = ''
 
+   # Takes in arguments from the command line
    try:
       opts, args = getopt.getopt(argv,"hi:o:")
    except getopt.GetoptError:
@@ -23,9 +27,6 @@ def main(argv):
       elif opt in ("-o"):
          outputfile = arg
          mode.append(opt)
-   print('Input file is "', inputfile)
-   print('Output file is "', outputfile)
-   print(mode)
    decrypt()
    
 
