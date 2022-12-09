f0f361db4a48c7d8e1f4028609de04707f23ef81
Gamerz261 <52172630+Gamerz261@users.noreply.github.com>
2022-12-09 10:13:47 -0600

    Comment Comment Comment

--- Methods/dictionaryAttack.py
+++ Methods/dictionaryAttack.py
@@ -1,7 +1,10 @@
 
 class DictionaryAttack:
-
+    
+    # Defines dictionary as a big variable
     content = ""
+    
+    # Color Codes!
     white = "\033[38;5;252m"
     pink = "\033[38;5;5m"
     red = "\033[38;5;1m"
@@ -10,25 +13,32 @@ class DictionaryAttack:
     blue = "\033[38;5;4m"
     purple = "\033[38;5;20m"
     
-
+    # Initialize the function
     def __init__(self):
         print(self.blue+"Running Dictionary Attack")
-
+    
+    # Return the size of the dictionary file
     def dictSize(self):
+        # Open the dictionary file and store it in content
         with open(r"bigDict.txt", 'r') as pg:
             self.content = pg.readlines()
             dictSize = len(self.content)
-        #print(self.content)
+        # Returns the length of content
         return dictSize
-
+    
+    # Check to see if a password is in the dictionary
     def check(self, password):
+        # Adds a new line seperator to the end of the password (because why truncate the whole dictionary 😔)
         password += "\n"
+        # Defines the size of the dictionary to report in the print statement
         size = DictionaryAttack.dictSize(self)
+        # If the password is found in the dictionary, print the fun stuff 😄, otherwise print the sad stuff 🥲
         if password in self.content:
             print(self.green+"Password found in the top " + str(size) + " most common passwords!")
             print("Password: "+ self.white + password, end='')
         else:
             print("That password is not in the top " + str(size) + " passwords.")
-
+    
+    # For use in other methods, return plaintext for the password at the intex requested
     def list(self, index):
         return self.content[index].strip()
