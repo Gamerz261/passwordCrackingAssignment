03994cb899bad42d3a8857ebf5e930a76243a67e
Gamerz261 <52172630+Gamerz261@users.noreply.github.com>
2022-12-09 09:55:57 -0600

    Finished Documentation
    COMMMENNNENEETTTNNNSEERTTS

--- Methods/md5.py
+++ Methods/md5.py
@@ -37,9 +37,10 @@ class MD5:
         self.data = md5(self.data.encode()).hexdigest()
         return self.data
     
-    # 
+    # Called by self.decrypt
     def bruteDecrypt(self):
         print(self.blue + "Beginning brute force cracking... This may take a while.")
+        # itterate through every 8 character password beginning with "1" and going through self.chars
         for i in range(1, 9):
             for letter in itertools.product(self.chars, repeat=i):
                 self.attempts += 1
@@ -56,32 +57,41 @@ class MD5:
         # Output Variables
         password = ''
         hashword = ''
+        
+        # Start the time function for analysis
         start = time.time()
         distance = ""
         attempts = 0
+        
+        # Pull the size of the dictionary from the DictionaryAttack method
         dictSize = DictionaryAttack.dictSize(self)
+        
+        # Itterate through the dictionary
         for count in range(dictSize):
             attempts += 1
+            
             password = DictionaryAttack.list(self, count).rstrip()
+            # Encode each dictionary entity
             hashword = hashlib.md5(password.encode('utf-8')).hexdigest()
             cracked = False
+            
+            # Compare the hashes from the dictionary to the input
             if hashword == self.data:
                 distance = time.time() - start
                 cracked = True
                 break
+        # If the hash matched the input, print the outcome to the command line
         if cracked:
             print(self.pink + "Password found in " + str(distance) + " seconds and " + str(attempts) + " attempts!")
             print(self.green + "Password:" + self.white + password)
             print(self.green + "MD5 Hash:" + self.white + hashword)
+        # If the hash DID NOT match any of the hashes from the dictionary, prompt the user with the option to brute force the password
         else:
             print(self.red + "That password is not in the top " + str(dictSize) + " passwords.")
             user = input(self.blue + "Would you like to attempt to crack the password through brute force? [y/n]: ")
+            
+            # If the user selects y, call the brute force method (self.bruteDecrypt)
             if user == 'y':
                 self.bruteDecrypt()
             else:
                 return
-
-# Experimental
-# crypt = MD5("Hello, world!")
-# print(crypt.encrypt())
-# print(crypt.decrypt("Hello, world!"))
